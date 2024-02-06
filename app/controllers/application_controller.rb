2cc60aa1268b59300d4b54624cd6b2061257b047
Washington Silva <w-osilva@hotmail.com>
2024-02-06 11:58:10 -0300

    feat: Add render_csv and render_json methods to ApplicationController
    This commit adds two new methods, `render_csv` and `render_json`, to the
    `ApplicationController` class.
    
    These methods are intended to provide convenient ways to render CSV and JSON
    data in the application.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def render_csv(collection, filename:)
+    require 'csv'
+
+    data = CSV.generate do |csv|
+      csv << collection.column_names
+
+      collection.find_each do |record|
+        csv << record.attributes.values
+      end
+    end
+
+    send_data(data, filename: filename, type: 'text/csv', disposition: 'attachment')
+  end
+
+  def render_json(collection, **)
+    render(json: collection, **)
+  end
 end
