78800116028ed1771a97b2373acb3527b2b651f0
Washington Silva <w-osilva@hotmail.com>
2024-02-06 17:42:54 -0300

    feat: Add scopes for supported_by and opposed_by in Bill model
    This commit adds two new scopes to the Bill model: supported_by and
    opposed_by. These scopes allow filtering bills based on the votes
    given by a specific legislator.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,18 @@
 class Bill < ApplicationRecord
   belongs_to :sponsor,
              class_name: 'Legislator'
+  has_many :votes
+  has_many :vote_results, through: :votes
 
   validates :title, presence: true
+
+  scope :supported_by, ->(legislator) do
+    includes(:vote_results)
+      .where(vote_results: { legislator: legislator, vote_type: 'yea' })
+  end
+
+  scope :opposed_by, ->(legislator) do
+    includes(:vote_results)
+      .where(vote_results: { legislator: legislator, vote_type: 'nay' })
+  end
 end
